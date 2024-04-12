d9f59f31368c36e40766bc2d0b45ea656c39b880
vickybarbieri0 <129549411+vickybarbieri0@users.noreply.github.com>
2024-04-12 02:32:03 +0000

    Add files via upload

---
+++ QueMePongo.java
@@ -0,0 +1,20 @@
+class Atuendo{
+	List<Atuendo> prendas;
+
+}
+
+class Prenda{
+	Categoria Categoria;
+	String material;
+	Colores Color;
+
+}
+
+class Categoria{
+	String tipo;
+}
+
+class Colores{
+	String principal;
+	String secundario;
+}
